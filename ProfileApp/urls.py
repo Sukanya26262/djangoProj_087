4299fb5068550cd517186b55923b8836c3424972
Sukanya132544 <sukanya26262@gmail.com>
2023-01-23 14:15:08 +0700

    5 commit 15ม.ค.65

--- ProfileApp/urls.py
+++ ProfileApp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.http import HttpResponse
 from ProfileApp import views
 urlpatterns = [
-    path('', views.home, name="home"),
     path('history', views.history, name="history"),
     path('interests', views.interests , name='interests'),
     path('career', views.career , name= 'career'),
